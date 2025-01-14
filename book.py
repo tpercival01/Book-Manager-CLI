9677e70b87f4701fbade6db94da3fb46331fff88
tpercival01 <thomaspercival@live.co.uk>
2025-01-14 13:20:52 +0000

    Finished book and bookcase functions

--- book.py
+++ book.py
@@ -1,20 +1,27 @@
 class Book:
-    def __init__(self):
-        pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.desc = None
+        self.finished = None
+        self.total_pages = None
+        self.total_chapters = None
+        self.progress = None
+        self.id = sum([ord(x) for x in "".join([title, author])])
 
-    def create_book(self):
-        pass
-
-    def update_book(self):
-        pass
+    def update_book(self, arg, value):
+        match(arg):
+            case "title":
+                self.title = value
 
 class Bookcase:
     def __init__(self):
-        pass
+        self.books = {}
 
     def view_all_books(self):
-        pass
+        print("")
+        for i in self.books:
+            print(f"ID {i}: {self.books[i].title} - {self.books[i].author}\n")
 
-    def add_new_to_bookcase(self):
-        pass
-
+    def add_new_to_bookcase(self, id, book):
+        self.books[id] = book
